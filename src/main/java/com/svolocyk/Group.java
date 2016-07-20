92bd50864c824595b9fa78307ac8174b42fdbb78
Steven Volocyk <sevenex@gmail.com>
2016-07-19 19:39:25 -0500

    added Group and GroupResource java classes

---
+++ src/main/java/com/svolocyk/Group.java
@@ -0,0 +1,7 @@
+package com.svolocyk;
+
+/**
+ * Created by seven on 7/19/2016.
+ */
+public class Group {
+}
--- src/main/java/com/svolocyk/GroupResource.java
+++ src/main/java/com/svolocyk/GroupResource.java
@@ -1,7 +1,11 @@
 package com.svolocyk;
 
+import javax.ws.rs.Path;
+
 /**
  * Created by seven on 7/19/2016.
  */
+@Path("groups")
 public class GroupResource {
+
 }
