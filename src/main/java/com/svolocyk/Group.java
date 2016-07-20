0ecc06d6f98e2e60a5b457e31b3b50f31e00a292
Steven Volocyk <sevenex@gmail.com>
2016-07-19 20:03:25 -0500

    completed Group.java and updated hibernate class mapping

--- src/main/java/com/svolocyk/Group.java
+++ src/main/java/com/svolocyk/Group.java
@@ -1,7 +1,45 @@
 package com.svolocyk;
 
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
 /**
  * Created by seven on 7/19/2016.
  */
+
+@Entity
+@Table(name = "rest_group")
 public class Group {
+    @Id
+    @GeneratedValue(generator="increment")
+    @GenericGenerator(name="increment", strategy = "increment")
+    @Column(name = "id")
+    private int id;
+
+    @Column(name ="group_name")
+    private String groupName;
+
+    public Group(){}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+
+    @Override
+    public String toString(){
+        return new StringBuffer("Group Name: "). append(this.groupName).toString();
+    }
 }
