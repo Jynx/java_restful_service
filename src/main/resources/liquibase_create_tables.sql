ae4ae5d0ae3af0ce27bb17b8014bed76c53ef970
Steven Volocyk <sevenex@gmail.com>
2016-07-21 23:39:45 -0500

    removed typo

--- src/main/resources/liquibase_create_tables.sql
+++ src/main/resources/liquibase_create_tables.sql
@@ -6,14 +6,14 @@
      user_id varchar(50) NOT NULL,
      first_name varchar(50) NOT NULL,
      last_name varchar(50) NOT NULL,
-     CONSTRAINT rest_user1_pkey PRIMARY KEY (id)
+     CONSTRAINT rest_user_pkey PRIMARY KEY (id)
   );
 
   CREATE TABLE rest_group
   (
      id integer NOT NULL,
      group_name varchar(50) NOT NULL,
-     CONSTRAINT rest_group1_pkey PRIMARY KEY (id)
+     CONSTRAINT rest_group_pkey PRIMARY KEY (id)
   );
 
   CREATE TABLE rest_user_group_mapping
@@ -22,7 +22,7 @@
      group_name varchar(50) NOT NULL,
      user_id varchar(50) NOT NULL,
      user_unique_id integer NOT NULL,
-     CONSTRAINT rest_user_group_mapping1_pkey PRIMARY KEY (id)
+     CONSTRAINT rest_user_group_mapping_pkey PRIMARY KEY (id)
   );
 
 --rollback DROP TABLE SampleTable;
